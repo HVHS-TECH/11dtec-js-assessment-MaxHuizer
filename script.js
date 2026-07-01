c5bc1fdfa756e34b2e0097303c3806cd659d2cbc
MaxHuizer <24069mh@hvhs.school.nz>
2026-07-01 06:23:17 +0000

    Jul 1, 2026, 6:23 AM

--- script.js
+++ script.js
@@ -63,9 +63,6 @@ popup.classList.remove("add")
 const NAME_FIELD = document.getElementById("nameField"); userName = NAME_FIELD.value;
 const MONEY_FIELD = document.getElementById("moneyField"); pocketMoney = Number(MONEY_FIELD.value);
 
-ORDER_OUTPUT.innerHTML += "<p>Name:" + userName + "</p>"
-ORDER_OUTPUT.innerHTML += "<p>Your Money: $" + pocketMoney + "</p>"
-
 if(pocketMoney >= totalPrice){
     receiptPopup()
 
@@ -73,6 +70,14 @@ if(pocketMoney >= totalPrice){
     alert("Sorry, you can't afford your meal")
     completeOrderReturn()
     }
+
+if (userName == "") {
+    alert("Please enter a valid name");
+    completeOrderReturn();
+}
+
+ORDER_OUTPUT.innerHTML += "<p>Name:" + userName + "</p>"
+ORDER_OUTPUT.innerHTML += "<p>Your Money: $" + pocketMoney + "</p>"
 }
 
 
